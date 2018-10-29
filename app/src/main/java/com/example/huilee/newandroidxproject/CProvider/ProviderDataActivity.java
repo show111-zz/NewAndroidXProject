6ca2cb1da9a0962ebece6a1b705dc57e34c26e0d
huilee <huilee9999@gmail.com>
2018-10-28 22:59:19 -0400

    content provider

---
+++ app/src/main/java/com/example/huilee/newandroidxproject/CProvider/ProviderDataActivity.java
@@ -0,0 +1,20 @@
+package com.example.huilee.newandroidxproject.CProvider;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.huilee.newandroidxproject.R;
+
+/*
+ * Created by huilee
+ * Created date on 2018-10-28
+ */
+public class ProviderDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_permission_layout);
+    }
+}
